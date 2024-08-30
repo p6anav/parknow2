2c6661b4daf74a2b05c3a6c51a9c0d35990d0e2e
PRANAV <100874811+p6anav@users.noreply.github.com>
2024-08-30 21:11:29 +0530

    Update service.dart

--- Official website/P6anav/servi/service.dart
+++ Official website/P6anav/servi/service.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart'; // Import is required for Text and basic widgets
+
+class ServiceScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'service paru Screen',
+        style: TextStyle(color: Colors.white, fontSize: 24),
+      ),
+    );
+  }
+}
 
