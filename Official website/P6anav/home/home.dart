065177017e9cb913dadeca370100767a255a1a47
PRANAV <100874811+p6anav@users.noreply.github.com>
2024-08-30 21:10:58 +0530

    Update home.dart

--- Official website/P6anav/home/home.dart
+++ Official website/P6anav/home/home.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart'; // Import is required for Text and basic widgets
+
+class HomeScreen extends StatelessWidget {
+  final bool isSelected;
+
+  HomeScreen({required this.isSelected});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min, // Ensure the Column fits its content
+      crossAxisAlignment: CrossAxisAlignment.center, // Center the content horizontally
+      children: [
+        
+       
+      ],
+    );
+  }
+}
 
