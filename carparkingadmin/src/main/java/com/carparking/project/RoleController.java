ab9bbc7a96e851c0017c2421672516df763d2486
abhijith sreekumar <sreekumar.abhijith93@gmail.com>
2024-09-02 00:16:41 +0530

    [feat: cross orgin]
    [feat: cross orgin]

--- carparkingadmin/src/main/java/com/carparking/project/RoleController.java
+++ carparkingadmin/src/main/java/com/carparking/project/RoleController.java
@@ -5,6 +5,7 @@ import com.carparking.project.entities.RoleStaging;
 import com.carparking.project.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ public class RoleController {
     RoleService roleService;
 
     @GetMapping("/allRoles")
+    @CrossOrigin("*")
     public ResponseEntity<List<Role>> getAllRoles(){
         return ResponseEntity.ok(roleService.getAllRoles());
     }
